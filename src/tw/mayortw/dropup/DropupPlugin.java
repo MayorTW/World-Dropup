40cd12017bab375a42ccd1ff8a79ff151086c556
Rio6 <rio6.liu@gmail.com>
2019-04-09 21:59:21 -0700

    Zip and upload to dropbox works

--- src/tw/mayortw/dropup/DropupPlugin.java
+++ src/tw/mayortw/dropup/DropupPlugin.java
@@ -1,5 +1,10 @@
 package tw.mayortw.dropup;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -16,11 +21,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.World;
 
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+
 public class DropupPlugin extends JavaPlugin implements Listener {
 
-    private HashMap<UUID, Integer> backupTimers = new HashMap<>();
-
     private boolean hasMultiverse = true;
+
+    DbxClientV2 dbxClient;
+
+    private HashMap<UUID, Integer> backupTimers = new HashMap<>();
 
     private boolean disabled = false;
     private String disabledReason;
@@ -35,6 +46,14 @@ public class DropupPlugin extends JavaPlugin implements Listener {
         saveDefaultConfig();
 
         getServer().getPluginManager().registerEvents(this, this);
+
+        try {
+            dbxClient = new DbxClientV2(DbxRequestConfig.newBuilder("dropup/1.0").build(), getConfig().getString("dropbox_token"));
+            getLogger().info("Logged into " + dbxClient.users().getCurrentAccount().getName().getDisplayName());
+        } catch (DbxException e) {
+            getLogger().severe("Dropbox login error: " + e.getMessage());
+            getServer().getPluginManager().disablePlugin(this);
+        }
     }
 
     @Override
@@ -143,16 +162,34 @@ public class DropupPlugin extends JavaPlugin implements Listener {
 
         BukkitTask task = getServer().getScheduler().runTaskLater(this, () -> {
             backupWorld(world);
-
             backupTimers.put(id, -1);
         }, getConfig().getInt("min_interval", 300) * 20);
+
         backupTimers.put(id, task.getTaskId());
     }
 
     private void backupWorld(World world) {
-        getServer().broadcastMessage(String.format("[§e%s§f] Backing up §a%s", getName(), world.getName()));
+        getServer().broadcastMessage(String.format("[§e%s§f] 正在備份 §a%s", getName(), world.getName()));
+        // TODO backup async
 
         world.save();
-        // world.getWorldFolder()
+        File worldFolder = world.getWorldFolder();
+
+        try {
+            // Dropbox says to split the session with >150Mb files
+            // but they didn't me a good api for that
+            // so I'm not doing it
+            String uploadPath = String.format("%s/%s/%s.zip", getConfig().get("dropbox_path", "/WorldBackup"),
+                    world.getName(), LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss")));
+            OutputStream dbxOut = dbxClient.files().upload(uploadPath).getOutputStream(); // TODO limit the speed
+
+            // Zip and upload
+            FileUtil.zipFiles(dbxOut, worldFolder); // TODO Copy folder to temp location if zip directly doesn't work
+
+            getServer().broadcastMessage(String.format("[§e%s§f] §a%s§r 已備份到 %s", getName(), world.getName(), uploadPath));
+        } catch(IOException | DbxException e) {
+            getServer().broadcastMessage(String.format("[§e%s§f] 備份錯誤： §c%s", getName(), e.getMessage()));
+            e.printStackTrace();
+        }
     }
 }
--- src/tw/mayortw/dropup/FileUtil.java
+++ src/tw/mayortw/dropup/FileUtil.java
@@ -1,13 +1,15 @@
 package tw.mayortw.dropup;
 
 /*
- * Author: dianlemel（古錐)
+ * Made by dianlemel（古錐)
+ * Edited by R26
  */
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -18,90 +20,90 @@ import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
-import org.bukkit.Bukkit;
-
-import net.md_5.bungee.api.ChatColor;
-
 public class FileUtil {
-	
-	public static void zipFiles(File zip, File srcFiles) throws IOException {
-		ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zip));
-		ZipFiles(out, srcFiles, new StringBuilder());
-		out.close();
-	}
 
-	private static void ZipFiles(ZipOutputStream out, File srcFiles, StringBuilder zipFiles)
-			throws IOException {
-		if (srcFiles.isDirectory()) {
-			Stream.of(srcFiles.listFiles()).forEach(files -> {
-				try {
-					if (files.isDirectory()) {
-						StringBuilder zipFiless = new StringBuilder(zipFiles);
-						zipFiless.append(files.getName()).append(File.separator);
-						out.putNextEntry(new ZipEntry(zipFiless.toString()));
-						ZipFiles(out, files, zipFiless);
-					} else {
-						ZipFiles(out, files, zipFiles);
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			});
-		} else if (srcFiles.isFile()) {
-			byte[] buf = new byte[1024];
-			try (FileInputStream in = new FileInputStream(srcFiles)) {
-				out.putNextEntry(new ZipEntry(new StringBuilder(zipFiles).append(srcFiles.getName()).toString()));
-				int len;
-				while ((len = in.read(buf)) > 0) {
-					out.write(buf, 0, len);
-				}
-			} finally {
-				out.closeEntry();
-			}
-		}
-	}
+    public static void zipFiles(File zip, File srcFiles) throws IOException {
+        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zip));
+        ZipFiles(out, srcFiles, new StringBuilder());
+        out.close();
+    }
 
-	public static void copyDirectory(File source, File target) {
-		if (!target.exists()) {
-			target.mkdir();
-		}
-		Stream.of(source.listFiles()).forEach(f -> {
-			try {
-				if (f.isFile()) {
-					Files.copy(Paths.get(source.getAbsolutePath() + "/" + f.getName()),
-							Paths.get(target.getAbsolutePath() + "/" + f.getName()));
-				}
-				if (f.isDirectory()) {
-					File sourceDemo = new File(source.getAbsolutePath() + "/" + f.getName());
-					File destDemo = new File(target.getAbsolutePath() + "/" + f.getName());
-					copyDirectory(sourceDemo, destDemo);
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		});
-	}
+    public static void zipFiles(OutputStream zipOut, File srcFiles) throws IOException {
+        ZipOutputStream out = new ZipOutputStream(zipOut);
+        ZipFiles(out, srcFiles, new StringBuilder());
+        out.close();
+    }
 
-	public static void deleteDirectory(Path temp){
-		//Bukkit.getScheduler().runTaskAsynchronously(AutoBackup.main, () -> {
-			try {
-				Files.walkFileTree(temp, new SimpleFileVisitor<Path>() {
-					@Override
-					public FileVisitResult postVisitDirectory(Path file, IOException exc) throws IOException {
-						Files.delete(file);
-						return FileVisitResult.CONTINUE;
-					}
+    private static void ZipFiles(ZipOutputStream out, File srcFiles, StringBuilder zipFiles)
+            throws IOException {
+            if (srcFiles.isDirectory()) {
+                Stream.of(srcFiles.listFiles()).forEach(files -> {
+                    try {
+                        if (files.isDirectory()) {
+                            StringBuilder zipFiless = new StringBuilder(zipFiles);
+                            zipFiless.append(files.getName()).append(File.separator);
+                            out.putNextEntry(new ZipEntry(zipFiless.toString()));
+                            ZipFiles(out, files, zipFiless);
+                        } else {
+                            ZipFiles(out, files, zipFiles);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            } else if (srcFiles.isFile()) {
+                byte[] buf = new byte[1024];
+                try (FileInputStream in = new FileInputStream(srcFiles)) {
+                    out.putNextEntry(new ZipEntry(new StringBuilder(zipFiles).append(srcFiles.getName()).toString()));
+                    int len;
+                    while ((len = in.read(buf)) > 0) {
+                        out.write(buf, 0, len);
+                    }
+                } finally {
+                    out.closeEntry();
+                }
+            }
+    }
 
-					@Override
-					public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-						Files.delete(file);
-						return FileVisitResult.CONTINUE;
-					}
-				});
-			} catch (IOException e) {
-				//AutoBackup.broadcastMessage(ChatColor.RED.toString() + ChatColor.BOLD + "[AutoBackup] 刪除暫存檔" + temp + "失敗");	
-				e.printStackTrace();
-			}			
-		//});
-	}
+    public static void copyDirectory(File source, File target) {
+        if (!target.exists()) {
+            target.mkdir();
+        }
+        Stream.of(source.listFiles()).forEach(f -> {
+            try {
+                if (f.isFile()) {
+                    Files.copy(Paths.get(source.getAbsolutePath() + "/" + f.getName()),
+                            Paths.get(target.getAbsolutePath() + "/" + f.getName()));
+                }
+                if (f.isDirectory()) {
+                    File sourceDemo = new File(source.getAbsolutePath() + "/" + f.getName());
+                    File destDemo = new File(target.getAbsolutePath() + "/" + f.getName());
+                    copyDirectory(sourceDemo, destDemo);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public static void deleteDirectory(Path temp) {
+        try {
+            Files.walkFileTree(temp, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult postVisitDirectory(Path file, IOException exc) throws IOException {
+                    Files.delete(file);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Files.delete(file);
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            org.bukkit.Bukkit.broadcastMessage("§c[Dropup] 刪除暫存檔" + temp + "失敗");
+            e.printStackTrace();
+        }
+    }
 }
