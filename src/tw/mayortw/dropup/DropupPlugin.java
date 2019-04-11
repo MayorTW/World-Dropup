5adfd0f08f637be6b3ea8fb867ccfaf600508bf5
Rio6 <rio6.liu@gmail.com>
2019-04-10 19:43:29 -0700

    Run backup in background except when stopping

--- src/tw/mayortw/dropup/DropupPlugin.java
+++ src/tw/mayortw/dropup/DropupPlugin.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.UUID;
 
 import org.bukkit.command.*;
@@ -18,7 +18,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.World;
 
 import com.dropbox.core.DbxException;
@@ -31,7 +30,7 @@ public class DropupPlugin extends JavaPlugin implements Listener {
 
     DbxClientV2 dbxClient;
 
-    private HashMap<UUID, Integer> backupTimers = new HashMap<>();
+    private HashSet<UUID> awaitBackups = new HashSet<>();
 
     private boolean disabled = false;
     private String disabledReason;
@@ -77,7 +76,7 @@ public class DropupPlugin extends JavaPlugin implements Listener {
                         return true;
                     }
 
-                    backupWorld(world);
+                    backupWorld(world, true);
                     return true;
                 }
             case "restore":
@@ -152,44 +151,55 @@ public class DropupPlugin extends JavaPlugin implements Listener {
     }
 
     public void onDisable() {
-        // Execute all backup timers with full speed
+        // Execute all awaiting backups
+        for(UUID id : awaitBackups) {
+            backupWorld(getServer().getWorld(id), false);
+        }
+
         saveConfig();
     }
 
     private void backupWorldLater(World world) {
         UUID id = world.getUID();
-        if(backupTimers.getOrDefault(id, -1) >= 0) return;
+        if(awaitBackups.contains(id)) return;
 
-        BukkitTask task = getServer().getScheduler().runTaskLater(this, () -> {
-            backupWorld(world);
-            backupTimers.put(id, -1);
+        getServer().getScheduler().runTaskLater(this, () -> {
+            backupWorld(world, true);
         }, getConfig().getInt("min_interval", 300) * 20);
 
-        backupTimers.put(id, task.getTaskId());
+        awaitBackups.add(id);
     }
 
-    private void backupWorld(World world) {
-        getServer().broadcastMessage(String.format("[§e%s§f] 正在備份 §a%s", getName(), world.getName()));
-        // TODO backup async
+    private void backupWorld(World world, boolean async) {
+        Runnable task = () -> {
+            getServer().broadcastMessage(String.format("[§e%s§f] 正在備份 §a%s", getName(), world.getName()));
 
-        world.save();
-        File worldFolder = world.getWorldFolder();
+            world.save(); // Maybe save it in main thread?
+            File worldFolder = world.getWorldFolder();
 
-        try {
-            // Dropbox says to split the session with >150Mb files
-            // but they didn't me a good api for that
-            // so I'm not doing it
-            String uploadPath = String.format("%s/%s/%s.zip", getConfig().get("dropbox_path", "/WorldBackup"),
-                    world.getName(), LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss")));
-            OutputStream dbxOut = dbxClient.files().upload(uploadPath).getOutputStream(); // TODO limit the speed
+            try {
+                // Dropbox says to split the session with >150Mb files
+                // but they didn't me a good api for that
+                // so I'm not doing it
+                String uploadPath = String.format("%s/%s/%s.zip", getConfig().get("dropbox_path", "/WorldBackup"),
+                        world.getName(), LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss")));
+                OutputStream dbxOut = dbxClient.files().upload(uploadPath).getOutputStream(); // TODO limit the speed
 
-            // Zip and upload
-            FileUtil.zipFiles(dbxOut, worldFolder); // TODO Copy folder to temp location if zip directly doesn't work
+                // Zip and upload
+                FileUtil.zipFiles(dbxOut, worldFolder); // TODO Copy folder to temp location if zip directly doesn't work
 
-            getServer().broadcastMessage(String.format("[§e%s§f] §a%s§r 已備份到 %s", getName(), world.getName(), uploadPath));
-        } catch(IOException | DbxException e) {
-            getServer().broadcastMessage(String.format("[§e%s§f] 備份錯誤： §c%s", getName(), e.getMessage()));
-            e.printStackTrace();
-        }
+                getServer().broadcastMessage(String.format("[§e%s§f] §a%s§r 已備份到 %s", getName(), world.getName(), uploadPath));
+            } catch(IOException | DbxException e) {
+                getServer().broadcastMessage(String.format("[§e%s§f] 備份錯誤： §c%s", getName(), e.getMessage()));
+                e.printStackTrace();
+            }
+        };
+
+        if(async)
+            getServer().getScheduler().runTaskAsynchronously(this, task);
+        else
+            task.run();
+
+        awaitBackups.remove(world.getUID());
     }
 }
