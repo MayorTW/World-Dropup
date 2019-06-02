6c6780bc55667deb781f93ae0d12b2859bca9454
Rio6 <rio6.liu@gmail.com>
2019-06-01 19:36:49 -0700

    Need to override read(byte[], int, int) from FilterInputStream, too

--- src/tw/mayortw/dropup/util/LimitedInputStream.java
+++ src/tw/mayortw/dropup/util/LimitedInputStream.java
@@ -48,6 +48,34 @@ public class LimitedInputStream extends FilterInputStream {
         }
     }
 
+    // Using InputStream's read(byte[], int, int) method
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        if (b == null) {
+            throw new NullPointerException();
+        } else if (off < 0 || len < 0 || len > b.length - off) {
+            throw new IndexOutOfBoundsException();
+        } else if (len == 0) {
+            return 0;
+        }
+
+        int c = read();
+        if (c == -1) {
+            return -1;
+        }
+        b[off] = (byte)c;
+
+        int i = 1;
+        for (; i < len ; i++) {
+            c = read();
+            if (c == -1) {
+                break;
+            }
+            b[off + i] = (byte)c;
+        }
+        return i;
+    }
+
     /*
      * returns -2 if read should be waiting
      */
